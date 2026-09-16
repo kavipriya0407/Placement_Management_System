ef30463065c1c0cf5dd6106abac02d6e512b5d2b
Kavipriya E <kavidharani0417@gmail.com>
2026-09-16 20:20:29 +0530

    Configure Django for production deployment

--- backend/placement_project/settings.py
+++ backend/placement_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)l!siqzar$^cm@jp@5_ngxfbqty$q204hw!1#_*(7dyfr0#y25'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
